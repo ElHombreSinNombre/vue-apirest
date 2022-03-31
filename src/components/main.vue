09754fa0158d13055000fa38461fa9d27992e38d
asier.lara <asier.lara@versia.com>
2022-03-31 15:00:52 +0200

    Espacios eliminados

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,7 +3,8 @@
     <div class="w-96 rounded-xl shadow-md p-5 bg-white hover:shadow-lg text-justify">
      <!-- Texto por defecto -->
       <div v-if="searchInput <  3 && loading == false" class="text-center p-0.5 ">
-        <div>Introduzca una búsqueda para encontrar diferentes localizaciones</div><i class="fa-2x p-0.5  fa-solid fa-earth-europe"></i>
+        <div>Introduzca una búsqueda para encontrar diferentes localizaciones</div>
+        <i class="fa-2x p-0.5  fa-solid fa-earth-europe"></i>
       </div>
       <!-- Verificamos que el array no está vacio e iteramos -->
       <div v-if="countries && countries.length" class="overflow-y-auto max-h-80">
@@ -13,8 +14,7 @@
             v-for="(countries, index) in countries"
             :key="index"
             class="text-gray-400 hover:text-black cursor-pointer mx-6"
-            @click="openGoogleMap(countries.lat, countries.lon)"
-          >
+            @click="openGoogleMap(countries.lat, countries.lon)">
             <i class="fa-solid fa-circle-right"></i>
             {{ countries.display_name }}
           </li>
@@ -28,11 +28,11 @@
         <i class="fas fa-info-circle p-0.5"></i> Escribe {{ 3 - searchInput.length }} caracter más
       </div>
       <!-- Texto en caso de que la búsqueda no devuelva nada -->
-      <div v-if="countries.length == 0 && searchInput.length >= 3 && loading == false" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded text-center">
-        No hay resultados
+      <div v-if="countries.length == 0 && searchInput.length >= 3 && loading == false" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded text-center"> No hay resultados
       </div>
       <!-- Icono que indica al usuario que se está realizando una búsqueda -->
       <div v-if="searchInput.length >= 3 && loading == true" class="text-center p-0.5 " >
+        <div>Buscando...</div>
         <i class="fa-2x fas fa-spinner fa-spin p-0.5"></i>
       </div>
     </div>
